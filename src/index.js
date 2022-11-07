eea0502a8b34777bcba219e8d7d61b821dd42e95
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-07 09:41:31 -0700

    Update index.js
    * Minor fixes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // 2) get a reference to the div with ID root
-const el = document.getElementById('root');
+const el = document.getElementById('root'); // el = element
 
 // 3) tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
 // 4) create a component
 function App() {
-  return <h1>Hello world!<h1/>
+  return <h1>Hello world!</h1>
 }
 
 {/*// 5) show the component on the screen*/}
 root.render(<App />);
-
